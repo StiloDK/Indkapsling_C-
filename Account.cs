fad2a581e2a9287b123410e89b4debb7c5bf29f7
StiloDK <86182820+StiloDK@users.noreply.github.com>
2024-11-05 13:15:05 +0100

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indkapsling_C_
+{
+    internal class Owner
+    {
+        public int KundeID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Owner(int kundeID, string firstName, string lastName)
+        {
+            KundeID = kundeID;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+
+    internal class Account
+    {
+        public double Balance { get; private set; }
+        public int ID { get; set; }
+        public Owner AccountOwner { get; set; }
+
+        public Account(double initialBalance, int id, Owner owner)
+        {
+            Balance = initialBalance;
+            ID = 123456;
+            AccountOwner = owner;
+        }
+
+        private string UpdateMessege()
+        {
+            return $"Din konto er blevet opdateret. Der står nu kr. {Balance}.";
+        }
+
+        public string AddBalance(double amount)
+        {
+            Balance += amount;
+            return UpdateMessege();
+        }
+
+        public string RemoveBalance(double amount)
+        {
+            Balance -= amount;
+            return UpdateMessege();
+        }
+        private string UpdateMessage()
+        {
+            return $"Yoru account has been updated. There is now. {Balance:F2}";
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Indkapsling_C_;
+
+namespace Indkapsling_CSharp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Owner owner = new Owner(123456, "Marius", "Fogelgren");
+
+            Account account = new Account(20.1, 123456, owner);
+            Console.WriteLine($"Hej {owner.FullName}, din konto er oprettet.");
+
+            double depositAmount = 1000.0;
+            string depositMessage = account.AddBalance(depositAmount);
+            Console.WriteLine(depositMessage);
+
+            double withdrawAmount = 500.0;
+            string withdrawMessage = account.RemoveBalance(withdrawAmount);
+            Console.WriteLine(withdrawMessage);
+
+        }
+    }
+}
